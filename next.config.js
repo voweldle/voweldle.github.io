2d83def02872f25a2a59d368c1b1d7fa294fae87
Shubham Chandak <shubhamchandak94@gmail.com>
2023-04-01 11:25:36 -0700

    Initialize next js project

---
+++ next.config.js
@@ -0,0 +1,14 @@
+// @ts-check
+
+/**
+ * @type {import('next').NextConfig}
+ **/
+const nextConfig = {
+  output: 'export',
+  basePath: '',
+  experimental: {
+    appDir: true,
+  },
+}
+
+module.exports = nextConfig
