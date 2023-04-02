dff1fe87ee89b02444cdc29043e99d5b765a26e0
shubhamchandak94 <shubhamchandak94@gmail.com>
2023-04-02 00:39:15 +0000

    Deploying to gh-pages from @ voweldle/voweldle.github.io@6f39012541b196af2b41c2cc9309c63ec5668e02 🚀

---
+++ _next/static/d-asnHjs_wsZwz7A35xCe/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/8c4abc292788cdb0.css","static/chunks/pages/index-7594812d1484a568.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
