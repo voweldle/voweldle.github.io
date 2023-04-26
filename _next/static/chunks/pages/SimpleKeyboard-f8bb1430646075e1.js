1ae55f6e3649d700e3c6e13344a38e9f3d38cda2
shivanicharkha <52616928+shivanicharkha@users.noreply.github.com>
2023-04-26 23:56:46 +0000

    Deploying to gh-pages from @ voweldle/voweldle.github.io@14b38c2885d233da191d7264156dc80e859b6a96 🚀

---
+++ _next/static/chunks/pages/SimpleKeyboard-f8bb1430646075e1.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[357],{5762:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/SimpleKeyboard",function(){return u(9439)}])}},function(n){n.O(0,[274,846,774,888,179],function(){return n(n.s=5762)}),_N_E=n.O()}]);
